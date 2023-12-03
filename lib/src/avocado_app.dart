ce5cd356aff8220d339232069b94128993b4bee3
JoelEspinal <joel_espinal@claro.com.do>
2023-12-03 05:15:19 -0400

    Move scaffold to dashboard view

--- lib/src/avocado_app.dart
+++ lib/src/avocado_app.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/dashboard/view/dashboard_view.dart';
+
 class AvocadoApp extends StatelessWidget {
   const AvocadoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 180,
-                  width: 300,
-                  child: Card(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 4.0,
-                      vertical: 8.0,
-                    ),
-                    color: Colors.lightBlueAccent,
-                    elevation: 5,
-                    child: Text(""),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: DashboardView(),
     );
   }
 }
---
+++ lib/src/presentation/dashboard/view/dashboard_view.dart
@@ -0,0 +1,40 @@
+import 'package:avocato_visualization/src/presentation/dashboard/show_dashboard_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+class DashboardView extends StackedView<ShowDashboardViewModel> {
+  const DashboardView({super.key});
+
+  @override
+  Widget builder(
+      BuildContext context, ShowDashboardViewModel viewModel, Widget? child) {
+    return const Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 180,
+                width: 300,
+                child: Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 4.0,
+                    vertical: 8.0,
+                  ),
+                  color: Colors.lightBlueAccent,
+                  elevation: 5,
+                  child: Text(""),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  ShowDashboardViewModel viewModelBuilder(BuildContext context) {
+    return ShowDashboardViewModel();
+  }
+}
